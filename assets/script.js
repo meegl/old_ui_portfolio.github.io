4127185946a04bda39d634d282158a271374b233
vectorsize <vectorsize@gmail.com>
2024-09-03 19:01:01 +0200

    upload files

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+/*
+Author: Rude Mortensen
+Info: see /humans.txt
+*/
+(function () {
+  "use strict";
+
+  // Define your functions here
+
+  $(function () {
+
+    // Load them here
+
+  });
+
+})();
